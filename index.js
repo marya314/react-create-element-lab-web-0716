35f48a8e5a83e159ed4dbda991ba59ea7bbf682d
marya314 <mary.boyd314@gmail.com>
2016-09-22 17:00:09 -0400

    added h1, p, list react components

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Code React element here
+
+const title =
+    React.createElement('h1', {}, 'An Awesome Person');
+
+const tagline =
+    React.createElement('p', {}, 'Who is learning React');
+
+const interests =
+        React.createElement('ul', {className: 'me_interests'}, [
+
+            React.createElement('li', {}, 'JavaScript'),
+            React.createElement('li', {}, 'React'),
+            React.createElement('li', {}, 'Movies'),
+            React.createElement('li', {}, 'Ice cream')
+
+        ]);
+
+const meInReact = React.createElement('div', {className: 'me'}, title, tagline, interests);
+
+ReactDOM.render(meInReact, document.getElementById('main'));
